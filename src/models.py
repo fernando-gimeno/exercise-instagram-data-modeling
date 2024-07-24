1ceddacb25aa564c4a3d22128576f4661adb3b1c
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-07-24 20:30:01 +0000

    Bugs fixeds

--- src/models.py
+++ src/models.py
@@ -16,10 +16,10 @@ followers = Table('follower',
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key = True)
-    username = Column(String, nullable = False)
-    firstname = Column(String)
-    lastname = Column(String)
-    email = Column(String, unique = True)
+    username = Column(String(250), nullable = False)
+    firstname = Column(String(250))
+    lastname = Column(String(250))
+    email = Column(String(250), unique = True)
     followers = relationship('User', secondary=followers, lazy='subquery',
         backref=backref('users', lazy=True))
     
@@ -33,8 +33,8 @@ class Post(Base):
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key = True)
-    type = Column()
-    url = Column(String)
+    type = Column(String(250))
+    url = Column(String(250))
     post_id = Column(Integer, ForeignKey('post.id'), nullable = False)
     
     post = relationship("Post")
@@ -42,7 +42,7 @@ class Media(Base):
 class Comment(Base):
     __tablename__ = 'comment'
     id = Column(Integer, primary_key = True)
-    comment_text = Column(String)
+    comment_text = Column(String(250))
     author_id = Column(Integer, ForeignKey('user.id'), nullable = False)
     post_id = Column(Integer, ForeignKey('post.id'), nullable = False)
     
